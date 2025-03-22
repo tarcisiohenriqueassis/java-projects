8310034cd046d399fb24bd9b49274b049cd419f2
Your Name <you@example.com>
2025-03-22 09:29:30 -0300

    mudado o nome, e adicionado um comentario

--- Conta-Banco/src/ContaTeminal.java
+++ Conta-Banco/src/ContaTeminal.java
@@ -2,12 +2,13 @@ import java.math.BigDecimal;
 import java.util.Locale;
 import java.util.Scanner;
 
-public class App {
+public class ContaTeminal {
     public static void main(String[] args) throws Exception {
        
         Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
 
         System.out.println("Digite seu nome completo: ");
+        //nextLine serve para pegar o valor inteiro da linha 
         String nome = scanner.nextLine();
 
         System.out.println("Digite a sua agência: ");
@@ -20,10 +21,6 @@ public class App {
         BigDecimal saldo = scanner.nextBigDecimal();
 
         System.out.println("Olá "+nome+", obrigado por criar uma conta em nosso banco, sua agência é "+agencia+", conta "+numero+" e seu saldo é de "+saldo+", e ja esta disponivel para saque.");
-        
-
-
-
 
     }
 }
